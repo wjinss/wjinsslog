a6609b03a92db218bed316c3fbc4111beb3ae7c3
wjinss <aastu4343@gmail.com>
2026-04-17 16:28:48 +0900

    feat: 로딩 컴포넌트

---
+++ src/app/(public)/loading.tsx
@@ -0,0 +1,14 @@
+import { PageContainer } from "@/components/layout/page-container";
+import { LoaderCircle } from "lucide-react";
+
+export default function PublicLoading() {
+  return (
+    <PageContainer>
+      <div className=" flex h-screen w-screen flex-col items-center justify-center">
+        <div className="itmes-center mb-15 flex gap-4 animate-spin">
+          <LoaderCircle />
+        </div>
+      </div>
+    </PageContainer>
+  );
+}
