110565126926385440046f0bebf9fb11d31e8643
wjinss <aastu4343@gmail.com>
2026-04-20 11:53:56 +0900

    style: 로딩 컴포넌트 스타일링 일부 변경

--- src/app/(public)/loading.tsx
+++ src/app/(public)/loading.tsx
@@ -4,7 +4,7 @@ import { LoaderCircle } from "lucide-react";
 export default function PublicLoading() {
   return (
     <PageContainer>
-      <div className=" flex h-screen w-screen flex-col items-center justify-center">
+      <div className="flex h-screen flex-col items-center justify-center">
         <div className="itmes-center mb-15 flex gap-4 animate-spin">
           <LoaderCircle />
         </div>
