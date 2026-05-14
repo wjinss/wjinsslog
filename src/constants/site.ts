04651eb4970d4e238a66c8b52af5f6ee8ca05a33
wjinss <aastu4343@gmail.com>
2026-05-15 00:52:42 +0900

    feat: 공용 메타데이터 설정 - name - title - description - URL - OG img

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,5 +1,7 @@
 export const SITE_CONFIG = {
   name: "wjinsslog",
   title: "wjinsslog | Personal Dev Blog",
-  description: "개인 개발 블로그",
+  description: "프론트엔드와 웹 개발 경험을 기록하는 개인 개발 블로그",
+  url: process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000",
+  ogImage: "/og-image.png",
 } as const;
